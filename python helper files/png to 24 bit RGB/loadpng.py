424f68d80d0d84f1b660420f0cccadfea856fdf1
Jackson Ballard <98064661+Tamedbumble@users.noreply.github.com>
2024-05-02 01:58:21 -0500

    sprite text files init

--- python helper files/png to 24 bit RGB/loadpng.py
+++ python helper files/png to 24 bit RGB/loadpng.py
@@ -2,20 +2,21 @@ from PIL import Image
 import sys
   
 # Import an image from directory: 
-input_image = Image.open("Weapons Doom Alpha.png") 
-pixel_data = open("text_data.txt", "a")
+input_image = Image.open("sprites/shotgun-6.png") 
+pixel_data = open("shotgun-6.txt", "a")
   
 # Extracting pixel map: 
 pixel_map = input_image.load() 
   
 width, height = input_image.size 
-count = 0
+start = 0
+count = start
 
-for i in range(width): 
-    for j in range(height): 
+for i in range(height): 
+    for j in range(width): 
         
         # getting the RGB pixel value. 
-        r, g, b = input_image.getpixel((i, j)) 
+        r, g, b, p = input_image.getpixel((j, i)) 
         rs = str(format(r, '08b'))
         gs = str(format(g, '08b'))
         bs = str(format(b, '08b'))
