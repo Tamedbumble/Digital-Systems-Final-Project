cb490ed9f6ab655368e4a6f8b8c2fd5a3315ca1b
Jackson Ballard <98064661+Tamedbumble@users.noreply.github.com>
2024-05-02 01:08:42 -0500

    python png to HDL converter

---
+++ loadpng.py
@@ -0,0 +1,37 @@
+from PIL import Image 
+import sys
+  
+# Import an image from directory: 
+input_image = Image.open("Weapons Doom Alpha.png") 
+pixel_data = open("text_data.txt", "a")
+  
+# Extracting pixel map: 
+pixel_map = input_image.load() 
+  
+# Extracting the width and height  
+# of the image: 
+width, height = input_image.size 
+count = 0
+# taking half of the width: 
+for i in range(width): 
+    for j in range(height): 
+        
+        # getting the RGB pixel value. 
+        r, g, b = input_image.getpixel((i, j)) 
+        rs = str(format(r, '08b'))
+        gs = str(format(g, '08b'))
+        bs = str(format(b, '08b'))
+          
+        # if i == 189 and j == 118:
+        # print("16'd",count, ":  memContents =  24'b", rs + gs + bs, " ;", sep = '')
+        data = "16'd" + str(count) + ":  memContents =  24'b" + rs + gs + bs +  " ;"
+        pixel_data.writelines(data + "\n")
+        count = count + 1
+  
+# Saving the final output 
+# as "grayscale.png": 
+# input_image.save("test.png", format="png") 
+pixel_data.close()
+  
+# use input_image.show() to see the image on the 
+# output screen. 
